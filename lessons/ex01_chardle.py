d5aba5445832161c0fc2d2dc24197c7ae5bbad5d
Caroline Bryan <cgbryan@unc.edu>
2023-09-12 10:45:56 -0400

    first submission for ex01

--- lessons/ex01_chardle.py
+++ lessons/ex01_chardle.py
@@ -1,11 +1,12 @@
 """EX01 - Chardle - something like wordle"""
 _author_ = "730667997"
 full_word: str = input("Enter a 5-character word: ")
-one_char: str = input("Enter a single character: ")
 
 if(len(full_word)!=5):
     print("Error: Word must contain 5 characters")
     exit()
+
+one_char: str = input("Enter a single character: ")
 
 if(len(one_char)!=1):
     print("Error: Character must be a single character")
@@ -15,27 +16,25 @@ print("Searching for " + one_char + " in " + full_word)
 
 counter: int = 0
 
-if(full_word(0)) == one_char:
+if(full_word[0]) == one_char:
     print(one_char + " found at index 0")
     counter+=1
-if(full_word(1)) == one_char:
+if(full_word[1]) == one_char:
     print(one_char + " found at index 1")
     counter+=1
-if(full_word(2)) == one_char:
+if(full_word[2]) == one_char:
     print(one_char + " found at index 2")
     counter+=1
-if(full_word(3)) == one_char:
+if(full_word[3]) == one_char:
     print(one_char + " found at index 3")
     counter+=1
-if(full_word(4)) == one_char:
+if(full_word[4]) == one_char:
     print(one_char + " found at index 4")
     counter+=1
 
-if(counter == 0):
+if (counter == 0):
     print("No instances of " + one_char + " found in " + full_word)
+elif (counter == 1):
+    print("1 instance of " + one_char + " found in " + full_word)
 else:
-    if (counter == 1):
-        print("1 instance of " + one_char + " found in " + full_word)
-    else:
-        if (counter > 1):
-            print(counter + " instances of " + one_char + " found in " + full_word)
+    print(str(counter) + " instances of " + one_char + " found in " + full_word)
