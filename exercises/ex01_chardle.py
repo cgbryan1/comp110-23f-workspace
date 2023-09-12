a2fe2b45ef36e2cc355212547e1c879a0f8c2868
Caroline Bryan <cgbryan@unc.edu>
2023-09-12 14:07:23 -0400

    FINAL EX01 WOOHOO

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,35 +2,35 @@
 __author__ = "730657997"
 full_word: str = input("Enter a 5-character word: ")
 
-if(len(full_word)!=5):
+if (len(full_word) != 5):
     print("Error: Word must contain 5 characters")
     exit()
 
 one_char: str = input("Enter a single character: ")
 
-if(len(one_char)!=1):
-    print("Error: Character must be a single character")
+if (len(one_char) != 1):
+    print("Error: Character must be a single character ")
     exit()
 
 print("Searching for " + one_char + " in " + full_word)
 
 counter: int = 0
 
-if(full_word[0]) == one_char:
+if (full_word[0]) == one_char:
     print(one_char + " found at index 0")
-    counter+=1
-if(full_word[1]) == one_char:
+    counter += 1 
+if (full_word[1]) == one_char:
     print(one_char + " found at index 1")
-    counter+=1
-if(full_word[2]) == one_char:
+    counter += 1 
+if (full_word[2]) == one_char:
     print(one_char + " found at index 2")
-    counter+=1
-if(full_word[3]) == one_char:
+    counter += 1 
+if (full_word[3]) == one_char:
     print(one_char + " found at index 3")
-    counter+=1
-if(full_word[4]) == one_char:
+    counter += 1 
+if (full_word[4]) == one_char:
     print(one_char + " found at index 4")
-    counter+=1
+    counter += 1 
 
 if (counter == 0):
     print("No instances of " + one_char + " found in " + full_word)
